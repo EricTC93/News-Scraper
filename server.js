79cbacdfd47a1af3124e611730a4107d21e06474
YETC7 <tyler_726@live.com>
2017-09-09 14:53:27 -0700

    Stores articles' title/body into database

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
 
+var request = require("request");
+var cheerio = require("cheerio");
+
 // Models
 var Article = require("./models/Article.js");
 
@@ -27,25 +30,64 @@ db.once("open", function() {
 	console.log("Mongoose connection successful.");
 });
 
-// Creates test article
-var testArticle = new Article({
-	title: "Title",
-	body: "Body"
-});
+// // Creates test article
+// var testArticle = new Article({
+// 	title: "Title",
+// 	body: "Body"
+// });
 
-// Saves test article to the database
-testArticle.save(function(err,doc){
-	if (err) {
-		console.log(err);
-	}
+// // Saves test article to the database
+// testArticle.save(function(err,doc){
+// 	if (err) {
+// 		console.log(err);
+// 	}
 
-	else {
-		console.log(doc);
-	}
-});
+// 	else {
+// 		console.log(doc);
+// 	}
+// });
 
 // Shows all articles in the database on the webpage
 app.get("/",function(req,res) {
+
+	request("https://www.nytimes.com/", function(err,response,html){
+		var $ = cheerio.load(html);
+
+		var results = [];
+
+		// $(".collection .story-heading > a").each(function(i,element) {
+		// 	// results.push($(element).children(".story-heading").children("a").attr("href"));
+		// 	// results.push(element);
+
+		// 	console.log(element.attr("text"));
+		// 	// console.log(element);
+		// });
+
+		// // console.log(results[0]);
+
+		$(".collection").each(function(i, element) {
+    		var title = $(element).find(".story-heading").find("a").text();
+    		var summary = $(element).find(".summary").text();
+
+    		// results.push({ title: title, body: summary });
+			// Creates test article
+			var newArticle = new Article({
+				title: title,
+				body: summary
+			});
+
+			// Saves new article to database if unique
+			newArticle.save(function(err,doc){
+				// if (err) {
+				// 	console.log(err);
+				// }
+			});
+
+			// console.log($(element).find(".story-heading").find("a").attr("href"));
+  		});
+
+  		// res.json(results);
+	});
 
 	Article.find({},function(err,found){
 		if (err) {
